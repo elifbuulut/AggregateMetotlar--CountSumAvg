6e481b42afd5474d61ce0c8e4f4f7d20096e2745
elifb <elifb@DESKTOP-R9ITCEQ>
2022-03-28 01:27:17 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace AggregateMetotları_CountSumAvg
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.LblToplamUrun = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtStokAdedi = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblStokAded = new System.Windows.Forms.Label();
+            this.lblFiyat = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(437, 313);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "BUL";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(294, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "TOPLAM ÜRÜN MİKTARI=";
+            // 
+            // LblToplamUrun
+            // 
+            this.LblToplamUrun.AutoSize = true;
+            this.LblToplamUrun.Location = new System.Drawing.Point(449, 39);
+            this.LblToplamUrun.Name = "LblToplamUrun";
+            this.LblToplamUrun.Size = new System.Drawing.Size(28, 13);
+            this.LblToplamUrun.TabIndex = 2;
+            this.LblToplamUrun.Text = ".......";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(294, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(119, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "TOPLAM STOK ADEDİ\r\n";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(449, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = ".......";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(449, 117);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(28, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = ".......";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(294, 117);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(136, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "BUZDOLABI STOK ADEDİ";
+            // 
+            // txtStokAdedi
+            // 
+            this.txtStokAdedi.Location = new System.Drawing.Point(500, 215);
+            this.txtStokAdedi.Name = "txtStokAdedi";
+            this.txtStokAdedi.Size = new System.Drawing.Size(100, 20);
+            this.txtStokAdedi.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(265, 215);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(153, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "GİRİLEN ÜRÜN STOK ADEDİ\r\n";
+            // 
+            // lblStokAded
+            // 
+            this.lblStokAded.AutoSize = true;
+            this.lblStokAded.Location = new System.Drawing.Point(635, 222);
+            this.lblStokAded.Name = "lblStokAded";
+            this.lblStokAded.Size = new System.Drawing.Size(28, 13);
+            this.lblStokAded.TabIndex = 10;
+            this.lblStokAded.Text = ".......";
+            // 
+            // lblFiyat
+            // 
+            this.lblFiyat.AutoSize = true;
+            this.lblFiyat.Location = new System.Drawing.Point(497, 275);
+            this.lblFiyat.Name = "lblFiyat";
+            this.lblFiyat.Size = new System.Drawing.Size(28, 13);
+            this.lblFiyat.TabIndex = 10;
+            this.lblFiyat.Text = ".......";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(265, 262);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(179, 13);
+            this.label9.TabIndex = 11;
+            this.label9.Text = "ÜRÜNLERİN  FİYAT ORTALAMASI\r\n";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.lblFiyat);
+            this.Controls.Add(this.lblStokAded);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtStokAdedi);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.LblToplamUrun);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label LblToplamUrun;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtStokAdedi;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblStokAded;
+        private System.Windows.Forms.Label lblFiyat;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AggregateMetotları_CountSumAvg
+{
+    public partial class Form1 : Form
+    {
+        Db_UrunlerEntities db = new Db_UrunlerEntities();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //toplam ürün miktarı
+            LblToplamUrun.Text = db.Tbl_Urunler.Count().ToString();
+
+            //Toplam Stok Adedi
+            label2.Text = db.Tbl_Urunler.Sum(q1 => q1.UrunStok).ToString();
+
+            //BUZDOLABI STOK ADEDİ
+            label4.Text = db.Tbl_Urunler.Where(q1 => q1.UrunAd == "Buzdolabı").
+                Sum(q1 => q1.UrunStok).ToString();
+
+            //TEXTBOXA GİRİLEN ÜRÜNÜN STOK ADEDİ
+            if (String.IsNullOrEmpty(txtStokAdedi.Text))
+            {
+                txtStokAdedi.Text = "BUZDOLABI";//EXCEPTION ICIN
+
+
+            }
+            else
+            {
+                var ad = txtStokAdedi.Text.ToUpper();
+                lblStokAded.Text = db.Tbl_Urunler.Where(q1 => q1.UrunAd == ad).Sum(q1 => q1.UrunStok).ToString();
+            }
+            
+
+            // ÜRÜNLERIN FİYAT ORTALAMASI
+        
+            lblFiyat.Text = db.Tbl_Urunler.Average(q2=>q2.UrunFiyat).ToString();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
